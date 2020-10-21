3e5dccfcf02790668b7ab35896fe62151e9f9f18
Hajruli Selmir <selmir.hajruli@trianon.ch>
2020-10-21 22:20:29 +0200

    Initial commit

---
+++ Controllers/CharactersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Epsic.Info3e.Rpg.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Epsic.Info3e.Rpg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharactersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var perso = new Character();
+            return Ok(perso);
+        }
+    }
+}
---
+++ Enums/RpgClass.cs
@@ -0,0 +1,9 @@
+namespace Epsic.Info3e.Rpg.Enums
+{
+    public enum RpgClass
+    {
+        Knight = 1,
+        Mage = 2,
+        Cleric = 3
+    }
+}
